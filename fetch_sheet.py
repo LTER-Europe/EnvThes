58b7f8ff91c4f3f554c1c4c3145cc214f170045c
Nikola Vasiljevic <35523348+niva83@users.noreply.github.com>
2021-01-08 14:39:58 +0100

    Update fetch_sheet.py

--- fetch_sheet.py
+++ fetch_sheet.py
@@ -16,31 +16,31 @@ with open("storage.json", "w") as json_file:
 
 
 
-data_file = "taxonomy.csv"
-sheet_id = '1zyEfkovkg66gWyzOgh8FucuE3CCVgHQCAjNEfx_kLmI'
-sheet_obj = Sheets.from_files('client.json', 'storage.json')
-sheet = sheet_obj.get(sheet_id)
+# data_file = "taxonomy.csv"
+# sheet_id = '1zyEfkovkg66gWyzOgh8FucuE3CCVgHQCAjNEfx_kLmI'
+# sheet_obj = Sheets.from_files('client.json', 'storage.json')
+# sheet = sheet_obj.get(sheet_id)
 
-sheet.sheets[0].to_csv(data_file, encoding='utf-8', dialect='excel')
+# sheet.sheets[0].to_csv(data_file, encoding='utf-8', dialect='excel')
 
-data_file_delimiter = ','
-largest_column_count = 0
+# data_file_delimiter = ','
+# largest_column_count = 0
 
-with open(data_file, 'r') as temp_f:
-    # Read the lines
-    lines = temp_f.readlines()
+# with open(data_file, 'r') as temp_f:
+#     # Read the lines
+#     lines = temp_f.readlines()
 
-    for l in lines:
-        # Count the column count for the current line
-        column_count = len(l.split(data_file_delimiter)) + 1
+#     for l in lines:
+#         # Count the column count for the current line
+#         column_count = len(l.split(data_file_delimiter)) + 1
 
-        # Set the new most column count
-        largest_column_count = column_count if largest_column_count < column_count else largest_column_count
+#         # Set the new most column count
+#         largest_column_count = column_count if largest_column_count < column_count else largest_column_count
 
-# Close file
-temp_f.close()
+# # Close file
+# temp_f.close()
 
 
-column_names = [i for i in range(0, largest_column_count)]
-df = pd.read_csv(data_file, header=None, delimiter=data_file_delimiter, names=column_names)
-df.to_excel('taxonomy.xlsx',index=False)
+# column_names = [i for i in range(0, largest_column_count)]
+# df = pd.read_csv(data_file, header=None, delimiter=data_file_delimiter, names=column_names)
+# df.to_excel('taxonomy.xlsx',index=False)
