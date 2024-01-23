92a096ad67a21a84099ffc897c9bcde893f35ed0
Will Bolton <wilbol@ceh.ac.uk>
2024-01-23 16:12:26 +0000

    Use API key instead of OAuth credentials

--- src/sheet2xls.py
+++ src/sheet2xls.py
@@ -4,19 +4,10 @@ import os
 import json
 
 
-client = json.loads(os.environ["CLIENT"])
-storage = json.loads(os.environ["STORAGE"])
-
-with open("client.json", "w") as json_file:
-    json.dump(client, json_file, indent=4)
-
-with open("storage.json", "w") as json_file:
-    json.dump(storage, json_file, indent=4)
-
-
 file_name = os.environ["FILE_NAME"]
 sheet_id = os.environ["SHEET_ID"]
-sheet_obj = Sheets.from_files("client.json", "storage.json")
+
+sheet_obj = Sheets.from_developer_key(os.environ["GOOGLE_API_KEY"])
 sheet = sheet_obj.get(sheet_id)
 sheet.sheets[0].to_csv(file_name + ".csv", encoding="utf-8", dialect="excel")
 txt_delimiter = ","
@@ -33,12 +24,8 @@ with open(file_name + ".csv", "r") as temp_f:
         )
 temp_f.close()
 
-
 column_names = [i for i in range(0, largest_column_count)]
 df = pd.read_csv(
     file_name + ".csv", header=None, delimiter=txt_delimiter, names=column_names
 )
 df.to_excel(file_name + ".xlsx", index=False, header=False)
-
-os.remove("client.json")
-os.remove("storage.json")
