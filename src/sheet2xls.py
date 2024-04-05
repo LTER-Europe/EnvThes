6cc901bddc68a6151d7d125cd18ed29102d3aef0
Anne Fouilloux <annef@simula.no>
2024-04-05 11:39:17 +0200

    update sheet2xls.py too

--- src/sheet2xls.py
+++ src/sheet2xls.py
@@ -1,15 +1,32 @@
-from gsheets import Sheets
-import pandas as pd
 import os
-import json
+import requests
+import sys
+import pandas as pd
+
+def getGoogleSeet(spreadsheet_id, outDir, outFile):
+  
+  url = f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=csv'
+  response = requests.get(url)
+  if response.status_code == 200:
+    filepath = os.path.join(outDir, outFile)
+    with open(filepath, 'wb') as f:
+      f.write(response.content)
+      print('CSV file saved to: {}'.format(filepath))    
+  else:
+    print(f'Error downloading Google Sheet: {response.status_code}')
+    sys.exit(1)
 
 
-file_name = os.environ["FILE_NAME"]
+##############################################
+
+file_name = os.environ['FILE_NAME']
 sheet_id = os.environ["SHEET_ID"]
 
-sheet_obj = Sheets.from_developer_key(os.environ["GOOGLE_API_KEY"])
-sheet = sheet_obj.get(sheet_id)
-sheet.sheets[0].to_csv(file_name + ".csv", encoding="utf-8", dialect="excel")
+outDir = './'
+
+os.makedirs(outDir, exist_ok = True)
+filepath = getGoogleSeet(sheet_id, outDir, file_name + ".csv")
+
 txt_delimiter = ","
 
 largest_column_count = 0
@@ -24,8 +41,12 @@ with open(file_name + ".csv", "r") as temp_f:
         )
 temp_f.close()
 
+
 column_names = [i for i in range(0, largest_column_count)]
+print("column names = ", column_names)
 df = pd.read_csv(
     file_name + ".csv", header=None, delimiter=txt_delimiter, names=column_names
 )
 df.to_excel(file_name + ".xlsx", index=False, header=False)
+
+sys.exit(0); ## success
